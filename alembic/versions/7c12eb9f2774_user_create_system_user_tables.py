752f613122e3d2b1328415fcdc47f1b564ee0184
shaheeqa <shaheeqa@plexydesk.com>
2021-06-12 16:46:08 +0530

    empty user table

---
+++ alembic/versions/7c12eb9f2774_user_create_system_user_tables.py
@@ -0,0 +1,24 @@
+"""user: create system user tables
+
+Revision ID: 7c12eb9f2774
+Revises: 
+Create Date: 2021-06-12 16:45:25.830532
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7c12eb9f2774'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
